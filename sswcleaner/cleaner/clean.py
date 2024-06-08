2bbb83946422951884e7687501587ff4db6bc57c
Brian Msane <msanebrianboss@gmail.com>
2024-06-08 02:00:47 +0200

    fixed bug

--- sswcleaner/cleaner/clean.py
+++ sswcleaner/cleaner/clean.py
@@ -32,17 +32,20 @@ class TextPreprocessor:
 
 
     def __init__(self):
-        self.stopwords = list(set(stopwords.words('english')))
-        self.stopwords.extend(stop.STOPWORDS)
+        try:
+            self.stopwords = list(set(stopwords.words('english')))
+            self.stopwords.extend(stop.STOPWORDS)
 
-        path = '../data/'
-        slang_data = pd.read_csv(path + 'Slang.csv', usecols=['slang', 'meaning'])
-        typo_data = pd.read_csv(path + 'Typo.csv', usecols=['typo', 'correct_word'])
-        borrowed_data = pd.read_csv(path + 'Borrowed.csv', usecols=['foreign', 'siswati_version'])
+            path = '../data/'
+            slang_data = pd.read_csv(path + 'Slang.csv', usecols=['slang', 'meaning'])
+            typo_data = pd.read_csv(path + 'Typo.csv', usecols=['typo', 'correct_word'])
+            borrowed_data = pd.read_csv(path + 'Borrowed.csv', usecols=['foreign', 'siswati_version'])
 
-        self.slang_correction_map = dict(zip(slang_data['slang'].astype(str), slang_data['meaning'].astype(str)))
-        self.typo_correction_map = dict(zip(typo_data['typo'].astype(str), typo_data['correct_word'].astype(str)))
-        self.borrowed_correction_map = dict(zip(borrowed_data['foreign'].astype(str), borrowed_data['siswati_version'].astype(str)))
+            self.slang_correction_map = dict(zip(slang_data['slang'].astype(str), slang_data['meaning'].astype(str)))
+            self.typo_correction_map = dict(zip(typo_data['typo'].astype(str), typo_data['correct_word'].astype(str)))
+            self.borrowed_correction_map = dict(zip(borrowed_data['foreign'].astype(str), borrowed_data['siswati_version'].astype(str)))
+        except Exception as e:
+            raise e
 
 
     def clean_text(self, text: str) -> str:
@@ -68,9 +71,7 @@ class TextPreprocessor:
         """
 
         try:            
-
             text = text.lower()
-            text = self.remove_numbers(text)
             text = self.remove_extra_spaces(text)
             text = self.remove_emojis(text)
             text = self.remove_emoticons(text)
@@ -84,41 +85,61 @@ class TextPreprocessor:
             text = self.resolve_slang(text)
             text = self.remove_stopwords(text)
             text = self.resolve_borrowed_words(text)
+            text = self.remove_numbers(text)
             return text
-
         except Exception as e:
-            logging.info("Exception occurred while cleaning text! %s", e)
-            return text
+            raise e
 
 
     def remove_punctuation_marks(self, text: str) -> str:
         return text.translate(str.maketrans('', '', string.punctuation))
 
     def remove_numbers(self, text: str) -> str:
-        return re.sub(r'\d', '', text)
+        try:
+            return re.sub(r'\d', '', text)
+        except Exception as e:
+            raise e
 
     def remove_extra_spaces(self, text: str) -> str:
-        return ' '.join(text.split())
+        try:
+            return ' '.join(text.split())
+        except Exception as e:
+            raise e
 
     def remove_emojis(self, text: str) -> str:
-        return text.encode('ascii', 'ignore').decode('ascii')
+        try:
+            return text.encode('ascii', 'ignore').decode('ascii')
+        except Exception as e:
+            raise e
 
     def remove_emoticons(self, text: str) -> str:
-        emoticon_pattern = r'[:;=][\-\^]?[D\)\]\(\]/\\OpP]'
-        return re.sub(emoticon_pattern, '', text)
+        try:
+            emoticon_pattern = r'[:;=][\-\^]?[D\)\]\(\]/\\OpP]'
+            return re.sub(emoticon_pattern, '', text)
+        except Exception as e:
+            raise e
 
     def remove_tags(self, text: str) -> str:
-        pattern = re.compile(r'<.*?>')
-        cleaned_text = re.sub(pattern, '', text)
-        return cleaned_text
+        try:
+            pattern = re.compile(r'<.*?>')
+            cleaned_text = re.sub(pattern, '', text)
+            return cleaned_text
+        except Exception as e:
+            raise e
 
     def remove_links(self, text: str) -> str:
-        url_pattern = re.compile(r'https?://\S+|www\.\S+')
-        text = url_pattern.sub('', text)
-        return text
+        try:
+            url_pattern = re.compile(r'https?://\S+|www\.\S+')
+            text = url_pattern.sub('', text)
+            return text
+        except Exception as e:
+            raise e
 
     def remove_unicode_character(self, text:str) -> str:
-        return re.sub(r'[^\x00-\x7F]+', '', text)
+        try:
+            return re.sub(r'[^\x00-\x7F]+', '', text)
+        except Exception as e:
+            raise e
 
 
     def remove_stopwords(self, text: str) -> str:
@@ -131,9 +152,12 @@ class TextPreprocessor:
                 The text to be cleaned.
 
         """
-        words = word_tokenize(text)
-        filtered_words = [word for word in words if word not in self.stopwords]
-        return ' '.join(filtered_words)
+        try:
+            words = word_tokenize(text)
+            filtered_words = [word for word in words if word not in self.stopwords]
+            return ' '.join(filtered_words)
+        except Exception as e:
+            raise e
 
 
     def add_i_end(self, text: str) -> str:
@@ -148,10 +172,13 @@ class TextPreprocessor:
                 The text to be cleaned.
 
         """
-        vowels = {'a', 'e', 'i', 'o', 'u'}
-        words = text.split()
-        corrected_words = [word + 'i' if word[-1].lower() not in vowels else word for word in words]
-        return ' '.join(corrected_words)
+        try:
+            vowels = {'a', 'e', 'i', 'o', 'u'}
+            words = text.split()
+            corrected_words = [word + 'i' if word[-1].lower() not in vowels else word for word in words]
+            return ' '.join(corrected_words)
+        except Exception as e:
+            raise e
 
 
     def resolve_slang(self, text: str) -> str:
@@ -164,9 +191,17 @@ class TextPreprocessor:
                 The text to be cleaned.
 
         """
-        for slang, meaning in self.slang_correction_map.items():
-            text = text.replace(slang, meaning)
-        return text
+        try:
+            words = text.split()
+            corrected_words = []
+            for word in words:
+                if word in self.slang_correction_map:
+                    corrected_words.append(self.slang_correction_map[word])
+                else:
+                    corrected_words.append(word)
+            return ' '.join(corrected_words)
+        except Exception as e:
+            raise e
 
 
     def resolve_typos(self, text: str) -> str:
@@ -179,9 +214,17 @@ class TextPreprocessor:
                 The text to be cleaned.
 
         """
-        for typo, correction in self.typo_correction_map.items():
-            text = text.replace(typo, correction)
-        return text
+        try:
+            words = text.split()
+            corrected_words = []
+            for word in words:
+                if word in self.typo_correction_map:
+                    corrected_words.append(self.typo_correction_map[word])
+                else:
+                    corrected_words.append(word)
+            return ' '.join(corrected_words)
+        except Exception as e:
+            raise e
 
 
     def resolve_borrowed_words(self, text: str) -> str:
@@ -194,15 +237,24 @@ class TextPreprocessor:
                 The text to be cleaned.
 
         """
-        for borrowed, correction in self.borrowed_correction_map.items():
-            text = text.replace(borrowed, correction)
-        return text
+        try:
+            words = text.split()
+            corrected_words = []
+            for word in words:
+                if word in self.borrowed_correction_map:
+                    corrected_words.append(self.borrowed_correction_map[word])
+                else:
+                    corrected_words.append(word)
+            return ' '.join(corrected_words)
+        except Exception as e:
+            raise e
 
 
     def remove_adjacent_vowels(self, text: str) -> str:
         """
             Removes vowels that follow each other because in siSwati that is not the case and people
-            tend to do it when they express their emotions and feelings.
+            tend to do it when they express their emotions and feelings. These removed vowels are the
+            ones that are similar. 'aaaaaaaaeeeeiiiooouu' -> 'aeiou'
 
             Parameters:
             ----------
@@ -210,9 +262,12 @@ class TextPreprocessor:
                 The text to be cleaned.
 
         """
-        vowels = {'a', 'e', 'i', 'o', 'u'}
-        result = []
-        for i in range(len(text)):
-            if i == 0 or text[i] not in vowels or text[i] != text[i-1]:
-                result.append(text[i])
-        return ''.join(result)
+        try:
+            vowels = {'a', 'e', 'i', 'o', 'u'}
+            result = []
+            for i in range(len(text)):
+                if i == 0 or text[i] not in vowels or text[i] != text[i-1]:
+                    result.append(text[i])
+            return ''.join(result)
+        except Exception as e:
+            raise e
